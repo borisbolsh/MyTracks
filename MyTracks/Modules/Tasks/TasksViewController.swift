9e3e616ed70e4bd3c20181d353ae413abefea8d0
Boris Bo <bolsh.bo@yandex.ru>
2022-07-04 23:36:59 +0300

    add collectionView

--- MyTracks/Modules/Tasks/TasksViewController.swift
+++ MyTracks/Modules/Tasks/TasksViewController.swift
@@ -1,11 +1,41 @@
 import UIKit
 
 final class TasksViewController: UIViewController {
+	private lazy var collectionView = UICollectionView()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		view.backgroundColor = .bgc
+		setupSubviews()
+		configureCollectionView()
 	}
+
+	private func setupSubviews() {}
+
+	private func configureCollectionView() {
+		let collectionViewLayout = UICollectionViewFlowLayout()
+		self.collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+
+		collectionView.frame = view.bounds
+
+		view.addSubview(collectionView)
+		collectionView.dataSource = self
+		collectionView.delegate = self
+		collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+	}
+}
+
+// MARK: UICollectionViewDataSource
+extension TasksViewController: UICollectionViewDataSource {
+}
+
+// MARK: UICollectionViewDataSource
+extension TasksViewController: UICollectionViewDataSource {
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {}
+}
+
+// MARK: UICollectionViewDelegateFlowLayout
+extension TasksViewController: UICollectionViewDelegateFlowLayout {
 
 }
